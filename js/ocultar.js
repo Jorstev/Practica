9ac2510e9ca95abe5e1d167006703529eb3e8fe5
Jorstev <71983628+Jorstev@users.noreply.github.com>
2021-02-23 15:48:51 -0600

    cambios
    cambios

---
+++ js/ocultar.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var texto1 = document.getElementById('texto1').value;
+var texto2 = document.getElementById('texto2').value;
+var texto3 = document.getElementById('texto3').value;
+var texto4 = document.getElementById('texto4').value;
+var texto5 = document.getElementById('texto5').value;
+var texto6 = document.getElementById('texto6').value;
+var texto7 = document.getElementById('texto7').value;
+
+function mostrar1() {
+    texto1 = document.getElementById('texto1').style.display = 'block';
+
+}
+
+function mostrar2() {
+    texto2 = document.getElementById('texto2').style.display = 'block';
+}
+
+function mostrar3() {
+    texto3 = document.getElementById('texto3').style.display = 'block';
+}
+
+function mostrar4() {
+    texto4 = document.getElementById('texto4').style.display = 'block';
+}
+
+function mostrar5() {
+    texto5 = document.getElementById('texto5').style.display = 'block';
+}
+
+function mostrar6() {
+    texto6 = document.getElementById('texto6').style.display = 'block';
+}
+
+function mostrar7() {
+    texto7 = document.getElementById('texto7').style.display = 'block';
+}
